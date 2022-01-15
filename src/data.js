233cb9fef3a98f56a7fcc5cdc0dfb09d5a91d8eb
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-01-15 22:52:55 +0530

    completed slier

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const SliderItems = [
+    {
+        id: 1,
+        img: 'https://images.pexels.com/photos/972995/pexels-photo-972995.jpeg',
+        title: 'SUMMER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg: 'faf5fd',
+    },
+    {
+        id: 2,
+        img: 'https://images.pexels.com/photos/972995/pexels-photo-972995.jpeg',
+        title: 'WINTER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: 'https://images.pexels.com/photos/972995/pexels-photo-972995.jpeg',
+        title: 'POPULAR',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg: 'fbf0f4',
+    },
+]
