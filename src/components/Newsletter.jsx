83e80852aee6f70b552b3ca7e3ccaa960fb5168d
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-01-16 01:09:42 +0530

    completed newsletter section

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,57 @@
+import { Send } from '@material-ui/icons'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+const Title = styled.h1`
+    font-size: 70px;
+    margin-bottom: 20px;
+`
+const Description = styled.div`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+`
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+`
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+    outline: none;
+`
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
+`
+
+const Newsletter = () => {
+    return (
+        <Container>
+            <Title>Newsletter</Title>
+            <Description>Get timely updates fom your favourite products.</Description>
+            <InputContainer>
+                <Input placeholder='Your email'></Input>
+                <Button>
+                    <Send />
+                </Button>
+            </InputContainer>
+        </Container>
+    )
+}
+
+export default Newsletter
