b9e99de6beac9aaa9cf803ad7dcc0393a12cf7db
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-01-16 00:23:25 +0530

    completed categories section

--- src/data.js
+++ src/data.js
@@ -21,3 +21,21 @@ export const SliderItems = [
         bg: 'fbf0f4',
     },
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg",
+        title: "LIGHT JACKETS",
+    },
+]
